344a31871ed584703489b868c9785590c4aa86c6
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-09 21:10:39 +0900

    first commit

---
+++ src/main/kotlin/com/ader/springbatchpractice/SpringBatchPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.ader.springbatchpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchPracticeApplication>(*args)
+}

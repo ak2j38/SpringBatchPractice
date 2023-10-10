90b05ed03350c8879b58212b967009a44822df8f
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-10 23:00:35 +0900

    Batch 관련 환경설정 추가

--- src/main/kotlin/com/ader/springbatchpractice/SpringBatchPracticeApplication.kt
+++ src/main/kotlin/com/ader/springbatchpractice/SpringBatchPracticeApplication.kt
@@ -1,9 +1,11 @@
 package com.ader.springbatchpractice
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class SpringBatchPracticeApplication
 
 fun main(args: Array<String>) {

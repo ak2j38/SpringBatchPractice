eb51f4b68a1dedac3a1c8a861fc06b6011dac1a2
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-11 02:55:31 +0900

    배치 예제코드 추가

--- src/main/kotlin/com/ader/springbatchpractice/SpringBatchPracticeApplication.kt
+++ src/main/kotlin/com/ader/springbatchpractice/SpringBatchPracticeApplication.kt
@@ -1,11 +1,9 @@
 package com.ader.springbatchpractice
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableBatchProcessing
 class SpringBatchPracticeApplication
 
 fun main(args: Array<String>) {

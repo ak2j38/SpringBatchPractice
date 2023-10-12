dbfc290773d379ec6bc1544c245a9de6a018a6d2
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-12 23:52:27 +0900

    파라미터를 받는 배치 테스트를 위한 새로운 Job 추가

---
+++ src/main/kotlin/com/ader/springbatchpractice/job/ValidatedParamJobConfig.kt
@@ -0,0 +1,41 @@
+package com.ader.springbatchpractice.job
+
+import mu.KotlinLogging
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+private val logger = KotlinLogging.logger {}
+
+@Configuration
+class ValidatedParamJobConfig {
+
+  @Bean
+  fun job(
+    jobRepository: JobRepository,
+    transactionManager: PlatformTransactionManager
+  ): Job {
+    return JobBuilder("validatedParamJob", jobRepository)
+      .start(step(jobRepository, transactionManager))
+      .build()
+  }
+
+  @Bean
+  fun step(
+    jobRepository: JobRepository,
+    transactionManager: PlatformTransactionManager
+  ): Step {
+    return StepBuilder("collectStep", jobRepository)
+      .tasklet( {_,_ ->
+        logger.info { "validate, job" }
+        RepeatStatus.FINISHED
+      }, transactionManager)
+      .build()
+  }
+}
